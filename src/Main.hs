018d7572d7c9588ad96ae0e2652d9695c2dda6f9
faineance <faineance@users.noreply.github.com>
2015-12-31 16:31:14 +0000

    proper parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,41 @@
 module Main where
+import           Text.ParserCombinators.Parsec
+import Control.Monad hiding (sequence)
+import System.Environment (getArgs)
 
 data Sequence = Sequence { id :: String, bases :: [Base] }
                 deriving (Show)
-                
-data Base = Base { key :: Nucleobase, score :: Float }
+
+data Base = Base { key :: Nucleobase, score :: Char }
                 deriving (Eq, Show)
 
 
 data Nucleobase = A | C | G | T | U | N
                 deriving (Eq, Show, Read)
 
+nucleobase :: Parser Nucleobase
+nucleobase = msum [ t <$ char c | (c, t) <- types ] <|> N <$ anyChar
+     where types =
+             [ ('A', A)
+             , ('C', C)
+             , ('G', G)
+             , ('T', T)
+             , ('U', U)]
 
+
+sequence :: Parser Sequence
+sequence = do
+    char '@'
+    id <- many1 (noneOf "\n")
+    raw <- many1 nucleobase
+    char '+'
+    scores <- many1 (noneOf "\n")
+    return $ Sequence id (zipWith Base raw scores)
+
+process :: String -> IO ()
+process contents = return ()
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  [file] <- getArgs
+  process =<< readFile file
