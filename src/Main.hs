fe79ce444f6fde4683dbbff92670bcda211f2a28
faineance <faineance@users.noreply.github.com>
2015-12-31 16:58:33 +0000

    working parser

--- src/Main.hs
+++ src/Main.hs
@@ -9,12 +9,11 @@ data Sequence = Sequence { id :: String, bases :: [Base] }
 data Base = Base { key :: Nucleobase, score :: Char }
                 deriving (Eq, Show)
 
-
 data Nucleobase = A | C | G | T | U | N
                 deriving (Eq, Show, Read)
 
 nucleobase :: Parser Nucleobase
-nucleobase = msum [ t <$ char c | (c, t) <- types ] <|> N <$ anyChar
+nucleobase = msum [ t <$ char c | (c, t) <- types ] <|> N <$ noneOf "+"
      where types =
              [ ('A', A)
              , ('C', C)
@@ -29,11 +28,19 @@ sequence = do
     id <- many1 (noneOf "\n")
     raw <- many1 nucleobase
     char '+'
+    char '\n'
     scores <- many1 (noneOf "\n")
     return $ Sequence id (zipWith Base raw scores)
 
+
+parseSeq :: String -> Either ParseError [Sequence]
+parseSeq = parse (many1 Main.sequence) ""
+
+
 process :: String -> IO ()
-process contents = return ()
+process contents = case parseSeq contents of
+                        Left err -> print err
+                        Right program -> print program
 
 main :: IO ()
 main = do
